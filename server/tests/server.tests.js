087fc2410520b1950d823f7ce265fa1b4f0f0543
LAPTOP-6JT3A2QV\noel2 <noel25_1594@hotmail.com>
2019-01-15 16:13:38 -0600

    Added test cases for GET /todos/:id

--- server/tests/server.tests.js
+++ server/tests/server.tests.js
@@ -3,10 +3,13 @@ const supertest = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {objectId} =  require('.mongodb');
 
 const todos = [{
+  _id: new objectId(),
   text: 'First todo'
 }, {
+  _id: new objectId(),
   text: 'Second todo'
 }];
 
@@ -69,5 +72,35 @@ describe('GET /todos', ()=> {
       .end(done);
   });
 
+
+});
+
+describe('GET /todos/:id', ()=> {
+  it('should return todo doc', (done)=>{
+
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexSring()}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done)=>{
+    var hexId = new objectId().toHexSring();
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done)=>{
+
+    request(app)
+      .get(`/todos/123ABC`)
+      .expect(404)
+      .end(done);
+  });
 
 });
