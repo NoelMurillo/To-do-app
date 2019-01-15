60db77c53aeb3086657755b69f627a1b7975ba72
LAPTOP-6JT3A2QV\noel2 <noel25_1594@hotmail.com>
2019-01-15 15:37:05 -0600

    Added queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+
+const {objectId} = require('mongodb')
+const {mongoose} = require('./../server/db/mongoose')
+const {Todo} = require('./../server/models/todo')
+const {User} = require('./../server/models/user')
+
+// var id= '6c36422b94b44bd80312764f';
+//
+// if (!objectId.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos)=>{
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo)=>{
+//   console.log('Todo', todo);
+// });
+//
+// Todo.findById(id).then((todo)=>{
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by id', todo);
+// }).catch((e)=> console.log(e));
+
+var id="5c364972dfb1b24036a5f505";
+
+User.findById(id).then((todo)=>{
+  if (!todo) {
+    return console.log('Id not found');
+  }
+  console.log('Todo by id', todo);
+}).catch((e)=> console.log(e));
