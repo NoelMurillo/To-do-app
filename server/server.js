7555f9259aea717cf062bc6e2905cd7973130b0a
LAPTOP-6JT3A2QV\noel2 <noel25_1594@hotmail.com>
2019-01-22 15:45:27 -0600

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-
-var express = require('express');
-var bodyparser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const lodash = require('lodash');
+const express = require('express');
+const bodyparser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require ('./db/mongoose');
 var {Todo} = require('./models/todo')
@@ -67,6 +67,31 @@ app.delete('/todos/:id', (req, res)=>{
     res.status(400).send();
   });
 
+});
+
+app.patch('/todos/:id', (req, res)=>{
+  var id= req.params.id;
+  var body= _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed)&& body.completed) {
+    body.completedAt = new Date().getTime();
+  } else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo)=>{
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    return res.status(400).send();
+  });
 });
 
 app.listen(port, ()=> {
